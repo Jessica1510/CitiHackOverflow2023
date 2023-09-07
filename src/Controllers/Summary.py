702e77fa77832d6b36a6aeaa0f896cb9d10c8ba4
haosheng00 <64892358+haosheng00@users.noreply.github.com>
2023-09-07 21:57:11 +0800

    summary

--- src/Controllers/Summary.py
+++ src/Controllers/Summary.py
@@ -1,14 +1,13 @@
-from newsapi import NewsApiClient
 import os
 import openai
+from dotenv import load_dotenv
 
-OPENAI_API_KEY= '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'
-api = NewsApiClient(api_key = '86e5092894b74369aab1033df6f3a69f')
+load_dotenv()
+
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 openai.api_key= OPENAI_API_KEY
 
-# print(api.get_everything(q = 'apple AND finance',sort_by = 'popularity' ))
-
-
-chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user", "content": "give me a summary of appl "}])
-
-print(chat_completion)
+def generate_summary_message(ticker):
+    chat_completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user", "content": "give me a summary of " + ticker}])
+    summary_message = chat_completion.choices[0].message.content
+    return summary_message
