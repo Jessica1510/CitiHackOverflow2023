e56360a61e18246bb66bfb7c5e5fe7f023aea065
wei xian <45598522+killerrobbot@users.noreply.github.com>
2023-09-07 19:13:01 +0800

    update routing, missing routing for bottom navigation

--- frontend/src/pages/DataPage.js
+++ frontend/src/pages/DataPage.js
@@ -1,8 +1,49 @@
 import React from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+import {motion} from "framer-motion";
+import "../index.css"
+import BottomNavigation from "../components/BottomNavigation";
 
 const DataPage = () => {
+    let {ticker} = useParams();
+    const navigate = useNavigate();
+
+    // TODO: Replace this value with the actual endpoint
+    // fetch('/api/endpoint', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ ticker }),
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         // Handle response from backend
+    //         console.log(data);
+    //     })
+    //     .catch(error => {
+    //         // Handle error
+    //         console.error('Error:', error);
+    //         navigate("/");
+    //     });
+
     return (
         <div>
+            <motion.div
+                initial={{ y: '-100%' }} // Initial position (off-screen)
+                animate={{ y: '0' }} // Final position (on-screen)
+                transition={{ duration: 0.8, ease: 'easeOut' }} // Duration of animation
+                className={"header"}
+            >
+                <h1 className={"title"}>
+                    Welcome
+                </h1>
+                <SearchBar />
+            </motion.div>
+            <div className={"bottomNavigation"}>
+                <BottomNavigation />
+            </div>
         </div>
     );
 };
